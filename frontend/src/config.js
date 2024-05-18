06f7b88fef0ce74d3ae51ce7cc63f5c1ff75531c
prashantjain31 <prashantjain0501@gmail.com>
2024-05-18 17:24:56 +0530

    Minor changes in compose file and improved basic chat message

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,7 +1,7 @@
 import { createChatBotMessage } from 'react-chatbot-kit';
 
 const config = {
-  initialMessages: [createChatBotMessage(`Hello world`)],
+  initialMessages: [createChatBotMessage(`Hello there. You can type any question related to any video and I will be happy to help.`)],
 };
 
 export default config;
